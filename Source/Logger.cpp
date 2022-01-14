2ed1b5e7263493338490780eb639c6095c945df6
ShannonHG <s.hargrave4@gmail.com>
2022-01-13 19:55:36 -0500

    Disabled writing to log file.

--- Source/Logger.cpp
+++ Source/Logger.cpp
@@ -9,13 +9,11 @@
 
 namespace SHG
 {
-	const std::string LOG_FILE_NAME = "GBEmu.log";
 	const std::string INFO_MESSAGE_HEADER = "[INFO]";
 	const std::string WARNING_MESSAGE_HEADER = "[WARNING]";
 	const std::string ERROR_MESSAGE_HEADER = "[ERROR]";
 	const std::string SYSTEM_STATUS_MESSAGE_HEADER = "[SYSTEM]";
 
-	std::ofstream logFileStream;
 	std::vector<LogEntryEvent> logEntryCallbacks;
 
 	bool Logger::IsSystemEventLoggingEnabled = false;
@@ -45,17 +43,8 @@ namespace SHG
 		logEntryCallbacks.push_back(callback);
 	}
 
-	void Logger::InitLogFile()
-	{
-		logFileStream = std::ofstream(std::filesystem::current_path().string() + "/" + LOG_FILE_NAME, std::ios::out);
-	}
-
-	// TODO: Prevent the log file from getting too large.
 	void Logger::WriteMessage(std::string heading, const std::string& message, bool writeToConsole, const std::string& customHeader, LogMessageType messageType)
 	{
-		if (!logFileStream.is_open()) 
-			InitLogFile();
-
 		SystemTime::DateTime currentTime = SystemTime::GetCurrentTime();
 
 		std::stringstream formattedTime;
@@ -67,9 +56,6 @@ namespace SHG
 
 		if (writeToConsole) 
 			std::cout << formattedMessage << std::endl;
-
-		// TODO: std::endl will force the stream to be flushed and immediately written to file, consider removing this.
-		logFileStream << formattedMessage << std::endl;
 
 		for (LogEntryEvent callback : logEntryCallbacks)
 			callback(formattedMessage, messageType);
